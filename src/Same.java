c1d55ca536e4473408337b80a8001b5714025b7c
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-03-25 09:28:20 +0800

    modified

--- src/Same.java
+++ src/Same.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/**
+ 确定两个字符串乱序同构
+
+ 给定两个字符串，请编写程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。这里规定大小写为不同字符，且考虑字符串重点空格。
+
+ 给定一个string stringA和一个string stringB，请返回一个bool，代表两串是否重新排列后可相同。保证两串的长度都小于等于5000。
+
+ 这里有两个不同的方法实现，第一个实现先排序再比较，第二个是记录ascii码，再比较
+ * */
 public class Same {
     public static boolean checkSam1(String stringA, String stringB) {
         char[] c1=stringA.toCharArray();
