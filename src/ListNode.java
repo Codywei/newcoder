21646067a7bbaf2b7265427d529c88ed40c17ce2
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-04-05 15:58:40 +0800

    删除单向链表的节点

--- src/ListNode.java
+++ src/ListNode.java
@@ -8,4 +8,5 @@ public class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
 }
---
+++ src/Remove.java
@@ -0,0 +1,49 @@
+import java.util.*;
+/**
+ 实现一个算法，删除单向链表中间的某个结点，假定你只能访问该结点。
+
+ 给定带删除的节点，请执行删除操作，若该节点为尾节点，返回false，否则返回true
+
+ 这道题的主体思想是把后一个节点的值和next都赋给要删除的节点，然后删除后一个节点，其中第二种解法并不能改写节点中的值（注意题目给出的节点结构）
+
+ * */
+public class Remove {
+    public static boolean removeNode1(ListNode pNode) {
+        if (pNode.next==null){
+            return false;
+        }
+        pNode.val=pNode.next.val;
+        pNode.next=pNode.next.next;
+        return true;
+    }
+    public static boolean removeNode2(ListNode pNode) {
+        if (pNode.next==null){
+            return false;
+        }
+        pNode=pNode.next;
+        return true;
+    }
+    public static void main(String[]args){
+        ListNode head=new ListNode(1);
+        ListNode p1=new ListNode(2);
+        ListNode p2=new ListNode(3);
+        ListNode p3=new ListNode(4);
+        ListNode p4=new ListNode(5);
+        head.next=p1;
+        p1.next=p2;
+        p2.next=p3;
+        p3.next=p4;
+//        removeNode1(p3);
+//        while(head!=null){
+//            System.out.println(head.val);
+//            head =head.next;
+//
+//        }
+        removeNode2(p3);
+        while(head!=null){
+            System.out.println(head.val);
+            head =head.next;
+
+        }
+    }
+}
