456b19023973c61ae805688a5798a6c8734c8110
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-05-09 17:13:13 +0800

    num

--- src/CloseNumber.java
+++ src/CloseNumber.java
@@ -38,6 +38,7 @@ public class CloseNumber {
         for(int i=0;i<arr.length;i++){
          System.out.println(arr[i]);
         }
+
     }
 
 }
---
+++ src/Transform2.java
@@ -0,0 +1,20 @@
+/**
+ 整数转化
+
+ 编写一个函数，确定需要改变几个位，才能将整数A转变成整数B。
+
+ 给定两个整数int A，int B。请返回需要改变的数位个数。
+
+ 先求两个数的异或，再求异或值里有几个1.
+ * */
+public class Transform2 {
+    public static int calcCost(int A, int B) {
+        int result =A^B ;
+        return Integer.bitCount(result);
+    }
+    public static void main(String[]args){
+        int a=1;
+        int b=2;
+        System.out.println(calcCost(a,b));
+    }
+}
