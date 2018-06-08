a88bf53479dc249ab8e13f3dd5e4ccb3de546785
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-08 10:11:38 +0800

    约瑟夫问题

---
+++ src/Joseph.java
@@ -0,0 +1,29 @@
+/**
+ 约瑟夫问题I
+
+ 约瑟夫问题是一个非常著名的趣题，即由n个人坐成一圈，按顺时针由1开始给他们编号。然后由第一个人开始报数，数到m的人出局。现在需要求的是最后一个出局的人的编号。
+
+ 给定两个int n和m，代表游戏的人数。请返回最后一个出局的人的编号。保证n和m小于等于1000。
+
+ 设置一个链表，建立while循环，通过取余确定出局的人
+ * */
+import java.util.LinkedList;
+
+public class Joseph {
+    public static int getResult(int n, int m) {
+         LinkedList<Integer>list=new LinkedList<Integer>();
+         for(int i=1;i<=n;i++){
+               list.add(i);
+         }
+         int bt=0;
+        while(list.size()>1){
+            int delpos=(bt+m-1)%list.size();
+            list.remove(delpos);
+            bt=delpos;
+        }
+        return list.get(0);
+    }
+    public static void main(String[]args){
+        System.out.println(getResult(5,3));
+    }
+}
---
+++ src/Joseph2.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+/**
+ 约瑟夫问题II
+
+ 约瑟夫问题是一个著名的趣题。这里我们稍稍修改一下规则。有n个人站成一列。并从头到尾给他们编号，第一个人编号为1。然后从头开始报数，第一轮依次报1，2，1，2...然后报到2的人出局。
+
+ 接着第二轮再从上一轮最后一个报数的人开始依次报1，2，3，1，2，3...报到2，3的人出局。以此类推直到剩下以后一个人。现在需要求的即是这个人的编号。
+
+ 给定一个int n，代表游戏的人数。请返回最后一个人的编号.
+
+ 要注意两点 1.size会随着节点被移出而减小，要注意边界条件的判断 ；2.按题目要求要将最后一个元素移到链表的首位
+ * */
+public class Joseph2 {
+    public static int getResult(int n) {
+        if (n < 1) {
+            return -1;
+        }
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        int round = 2, i, curr = 0;
+        for (i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        while (list.size() > 1) {
+            i = 0;
+            //如果节点被移除size也同样减小，不用增加i，否则i++
+            while (list.size() > 1 && i < list.size()) {
+                curr = (curr + 1) % round;
+                if (curr != 1) {
+                    list.remove(i);
+                } else {
+                    i++;
+                }
+            }
+            // 下一轮的最大报数
+            round++;
+            // 将下一轮的位置置零
+            curr = 0;
+            // 将最后报数的元素移动到链表首部，即确保每次报数从链表首部开始。
+            int last = list.removeLast();
+            list.addFirst(last);
+
+        }
+        // 返回最后一个元素
+        return list.pop();
+    }
+
+    public static void main(String[]args){
+        System.out.println(getResult(5));
+    }
+}
