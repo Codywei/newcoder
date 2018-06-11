b30b8efa37364f74d4ab8e5053b29cd8441ad51b
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-11 15:19:07 +0800

    词频统计

---
+++ src/Frequency.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+/**
+ 词频统计
+
+ 请设计一个高效的方法，找出任意指定单词在一篇文章中的出现频数。
+
+ 给定一个string数组article和数组大小n及一个待统计单词word，请返回该单词在文章中的出现频数。保证文章的词数小于等于1000。
+
+ 建立一个hashmap，存放所有出现的词的词频。
+ * */
+
+public class Frequency {
+    public static int getFrequency(String[] article, int n, String word) {
+        HashMap <String,Integer>freq=new HashMap();
+        for(int i=0;i<n;i++){
+           if(!freq.containsKey(article[i])) {
+               freq.put(article[i],1);
+           }
+           else{
+               freq.put(article[i],freq.get(article[i])+1);
+           }
+        }
+
+        return freq.get(word)!=null?freq.get(word):0 ;
+    }
+    public static void main(String []args){
+        String []article={"12","12","23"};
+        System.out.println(getFrequency(article,3,"34"));
+    }
+}
---
+++ src/MaxSum.java
@@ -0,0 +1,32 @@
+/**
+ 最大连续数列和
+
+ 对于一个有正有负的整数数组，请找出总和最大的连续数列。
+
+ 给定一个int数组A和数组大小n，请返回最大的连续数列的和。保证n的大小小于等于3000。
+
+ 在一轮遍历中，记录每一段连续数列的和，取最大的和（此题连续正和连续负视为连续）
+ * */
+public class MaxSum {
+    public static int getMaxSum(int[] A, int n) {
+        int dp0 = A[0], max = A[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(dp0 + A[i] > A[i]) {
+                dp0 = dp0 + A[i];
+            }
+            else {
+                dp0 = A[i];
+            }
+            if(dp0 > max) {
+                max = dp0;
+            }
+        }
+        return max;
+
+    }
+    public static void main(String[]args){
+        int []A={1,2,3,-6,1};
+        System.out.println(getMaxSum(A,5));
+    }
+}
