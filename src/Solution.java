a522002f53dec03640ff3dd0b33ca8528d212232
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-18 14:28:14 +0800

    链表中的倒数第k个节点（链表）

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,11 @@
 /**
- * 链表中的倒数第k个节点
- *
- * 输入一个链表，输出该链表中倒数第k个结点。
- *
- * 思路：两个指针，先让第一个指针和第二个指针都指向头结点，然后再让第一个指正走(k-1)步，到达第k个节点。然后两个指针同时往后移动，当第一个结点到达末尾的时候，第二个结点所在位置就是倒数第k个节点了。
- *  （向第二种写法的同学学习）
+ 链表中的倒数第k个节点（链表）
+
+ 输入一个链表，输出该链表中倒数第k个结点。
+
+ 思路：两个指针，先让第一个指针和第二个指针都指向头结点，然后再让第一个指正走(k-1)步，到达第k个节点。然后两个指针同时往后移动，当第一个结点到达末尾的时候，第二个结点所在位置就是倒数第k个节点了。
+
+ （向第二种写法的同学学习）
  */
 
 public class Solution {
@@ -28,7 +29,7 @@ public class Solution {
     }
 
 
-    public static ListNode FindKthToTail2(ListNode head,int k) { //5,{1,2,3,4,5}
+    public static ListNode FindKthToTail2(ListNode head,int k) {
         ListNode p, q;
         p = q = head;
         int i = 0;
