f1ada5b5d9d58beaa4bf56add50b252b51ae4b32
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-18 13:58:13 +0800

    基本字符串压缩（字符串）

--- src/Zipper.java
+++ src/Zipper.java
@@ -1,16 +1,15 @@
-import java.util.*;
 /**
- 基本字符串压缩
+ 基本字符串压缩（字符串）
 
  利用字符重复出现的次数，编写一个方法，实现基本的字符串压缩功能。比如，字符串“aabcccccaaa”经压缩会变成“a2b1c5a3”。若压缩后的字符串没有变短，则返回原先的字符串。
 
  给定一个string iniString为待压缩的串(长度小于等于10000)，保证串内字符均由大小写英文字母组成，返回一个string，为所求的压缩后或未变化的串。
 
- 这道题主要注意最后一个字符不能遗漏，第二种解法比较省判断
+ 这道题主要注意最后一个字符不能遗漏，第二种解法比较省判断（利用高低位记录相对位置）
  * */
 public class Zipper {
     public static String zipString1(String iniString) {
-        // write code here
+
         int sum=1;
         char temp= ' ';
         StringBuffer str=new StringBuffer();
