e3634b120565932c7cbf752637f70cd907c89c76
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-20 10:56:02 +0800

    找出字符串（二分查找）

--- src/Finder3.java
+++ src/Finder3.java
@@ -1,3 +1,12 @@
+/**
+ 找出字符串（二分查找）
+
+ 有一个排过序的字符串数组，但是其中有插入了一些空字符串，请设计一个算法，找出给定字符串的位置。算法的查找部分的复杂度应该为log级别。
+
+ 给定一个string数组str,同时给定数组大小n和需要查找的string x，请返回该串的位置(位置从零开始)
+
+ 二分查找，跳过空格，注意头尾两端的空格判断
+ * */
 public class Finder3 {
     public static int findString(String[] str, int n, String x) {
         int left=0;
